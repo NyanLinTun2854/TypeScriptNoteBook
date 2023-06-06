6968fa34f94adc10cd8b28a0547021158af3f3f7
nyanlintun2854 <nyanlintun28504@gmail.com>
2023-06-06 17:20:56 +0630

    dd

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  type C = {
+    baba: string;
+    wawa: number;
+  };
+
+  let c: C = {
+    baba: "baba",
+    wawa: 23,
+  };
+
+  console.log(c);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className="heading">Taskify</span>
     </div>
   );
-}
+};
 
 export default App;
